7fa5ba4f028abedcb7bd912169cfe9614d035d66
biguga <adv06062001@gmail.com>
2023-04-26 12:01:46 +0500

    modulation

---
+++ js/util.js
@@ -0,0 +1,12 @@
+function getRandomInt (from, to) {
+  const lower = Math.ceil(Math.min(Math.abs(from), Math.abs(to)));
+  const upper = Math.floor(Math.max(Math.abs(from), Math.abs(to)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+export {getRandomInt};
